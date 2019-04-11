100af37e98d921f58bc7af4816384b5d67a6d24d
Stephanie Cappello <stephaniecappello@stephanies-mbp.attlocal.net>
2019-04-10 22:47:35 -0500

    welcome message passed

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,37 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setup(){
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void teardown(){
+        System.setOut(originalOut);
+    }
+
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void welcomeMsg() {
+        String welcomeMsg = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n";
+        String[] strings = null;
+        BibliotecaApp.main(strings);
+        assertEquals(welcomeMsg, outContent.toString());
     }
 }
